078390a54fe3f44bd49ce58373954697af1fc245
augusto <gutim160@gmail.com>
2022-01-28 16:46:08 -0300

    implantado com sucesso

--- quasar.conf.js
+++ quasar.conf.js
@@ -6,7 +6,7 @@
 // Configuration for your app
 // https://v1.quasar.dev/quasar-cli/quasar-conf-js
 
-module.exports = function (/* ctx */) {
+module.exports = function (ctx) {
   return {
     // https://v1.quasar.dev/quasar-cli/supporting-ts
     supportTS: false,
@@ -39,7 +39,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: "hash", // available values: 'hash', 'history'
-
+      distDir: ctx.mode.spa ? "public" : null,
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
